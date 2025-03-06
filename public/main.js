d27559b76ca5c958a7ec54ac098d1157ddfcb790
nahiwagaan <jetpadilla07@gmail.com>
2025-03-07 03:42:46 +0800

    public

--- public/main.js
+++ public/main.js
@@ -1,15 +1,16 @@
 import SPA from "./core/spa.js";
 
-
-// import './styles/common.css';
 import PageNotFound from "./pages/pageNotFound.js";
 import SignIn from "./pages/login.js";
+import SignUp from "./pages/signup.js";
 
 const app = new SPA({
     root: document.getElementById('app'),
     defaultRoute: PageNotFound,
 })
 
-app.add('/login', SignIn)
+app.add('/signIn', SignIn)
+app.add('/signup', SignUp)
+
 
 app.handleRouteChanges();
--- public/pages/signup.js
+++ public/pages/signup.js
@@ -1,5 +1,5 @@
-import CreateAccount from "../components/signUp";
-import Layout from "../layouts/default";
+import CreateAccount from "../components/signUp.js";
+import Layout from "../layouts/default.js";
 
 export default function SignUp(){
     const { main } = Layout(this.root)
