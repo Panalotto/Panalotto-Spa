e9eb38a3db67c327078c61c3fb19428829104dc0
nahiwagaan <jetpadilla07@gmail.com>
2025-03-14 09:00:42 +0800

    gen galaw galaw

--- public/components/login.js
+++ public/components/login.js
@@ -3,42 +3,42 @@ import '../styles/loginPage.css';
 
 export default function LogIn(root) {
     root.innerHTML = `
-    <div id="header-container">
-        <h1 id="headtext">PANALOTTO</h1>
-    </div>
+        <div class="custom-body">
+            <div id="header-container">
+                <h1 id="headtext">PANALOTTO</h1>
+            </div>
 
-    <div class="ball-container">
-        <div id="time">Next Draw: 60 seconds</div>
+            <div class="ball-container">
+                <div id="time">Next Draw: 60 seconds</div>
+                <div id="draw-numbers">
+                    <div class="number-box">10</div>
+                    <div class="number-box">10</div>
+                    <div class="number-box">10</div>
+                    <div class="number-box">10</div>
+                    <div class="number-box">10</div>
+                    <div class="number-box">10</div>
+                </div>
+            </div>
+            
+            <div id="prize-box">
+                <span id="dollar-icon">$ 5000</span>
+            </div>
 
-        <div id="draw-numbers">
-            <div class="number-box">10</div>
-            <div class="number-box">10</div>
-            <div class="number-box">10</div>
-            <div class="number-box">10</div>
-            <div class="number-box">10</div>
-            <div class="number-box">10</div>
+            <div id="loginPage">
+                <form enctype="multipart/form-data" id="signin">
+                    <h2 id="logintext">Login</h2>
+                    <label for="username"><b>Username</b></label>
+                    <input type="text" name="username" placeholder="Enter Username" required>
+
+                    <label for="password"><b>Password</b></label>
+                    <input type="password" name="password" placeholder="Enter Password" required>
+                    <span id="errorMessage" class="error-message"></span>
+
+                    <button type="submit">Login</button>
+                    <span class="crtAcct">Don't have an account? <a href="/signup">Register</a></span>
+                </form>
+            </div>
         </div>
-    </div>
-    
-    <div class="prize-box">
-        <span class="dollar-icon">$ 50000</span>
-    </div>
-
-    
-    <div id="loginPage">
-        <form enctype="multipart/form-data" id="signin">
-            <h2 id="logintext">Login</h2>
-            <label for="username"><b>Username</b></label>
-            <input type="text" name="username" placeholder="Enter Username" required>
-
-            <label for="password"><b>Password</b></label>
-            <input type="password" name="password" placeholder="Enter Password" required>
-            <span id="errorMessage" class="error-message"></span>
-
-            <button type="submit">Login</button>
-            <span class="crtAcct">Don't have an account? <a href="/signup">Register</a></span>
-        </form>
-    </div>
     `;
 
     // 🎯 WebSocket Countdown Update
